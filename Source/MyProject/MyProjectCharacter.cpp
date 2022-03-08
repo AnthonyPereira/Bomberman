866ada2d40e7595bde62463ea960dfe2deb7b060
AnthonyPereira <pereiraanthony62130@gmail.com>
2022-03-08 15:12:59 +0100

    camera down

--- Source/MyProject/MyProjectCharacter.cpp
+++ Source/MyProject/MyProjectCharacter.cpp
@@ -42,7 +42,7 @@ AMyProjectCharacter::AMyProjectCharacter()
 
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	/*CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 700.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
@@ -58,7 +58,7 @@ AMyProjectCharacter::AMyProjectCharacter()
 	CameraBoom->bInheritYaw = false;
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
-
+	*/
 
 	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("Health component"));
 

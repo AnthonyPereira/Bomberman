32ecf0f607f6fe8475125b00440d5d7ac4f49a6a
PierreLouisBouchez <pl.bouchez@outlook.fr>
2022-03-14 15:15:28 +0100

    fin de partie

--- Source/MyProject/MyProjectCharacter.cpp
+++ Source/MyProject/MyProjectCharacter.cpp
@@ -40,7 +40,7 @@ AMyProjectCharacter::AMyProjectCharacter()
 	GetCharacterMovement()->AirControl = 0.2f;
 	GetCharacterMovement()->MaxWalkSpeed = 200.f;
 	
-
+	/*
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 700.0f; // The camera follows at this distance behind the character	
@@ -51,13 +51,13 @@ AMyProjectCharacter::AMyProjectCharacter()
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName) ; // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
-
 	CameraBoom->bInheritPitch = false;
 	CameraBoom->bInheritRoll = false;
 	CameraBoom->bInheritYaw = false;
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
-	
+		*/
+
 
 	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("Health component"));
 

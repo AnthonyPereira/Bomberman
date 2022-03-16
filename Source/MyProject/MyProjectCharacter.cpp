7d6ebe84b0bca0c0750543650c1cb90dc0837695
PierreLouisBouchez <pl.bouchez@outlook.fr>
2022-03-16 18:45:09 +0100

    Maybe the last

--- Source/MyProject/MyProjectCharacter.cpp
+++ Source/MyProject/MyProjectCharacter.cpp
@@ -35,7 +35,7 @@ AMyProjectCharacter::AMyProjectCharacter()
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 750.f, 0.0f); // ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 	GetCharacterMovement()->MaxWalkSpeed = SpeedMax;
